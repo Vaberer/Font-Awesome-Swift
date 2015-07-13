91c46f2f658ff0789387d6f2201a1ba0a02e25e6
Patrik Vaberer <patrik.vaberer@gmail.com>
2015-07-13 13:17:45 +0200

    Initial Commit

---
+++ AF Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AF Swift
+//
+//  Created by Patrik Vaberer on 7/13/15.
+//  Copyright (c) 2015 Patrik Vaberer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
