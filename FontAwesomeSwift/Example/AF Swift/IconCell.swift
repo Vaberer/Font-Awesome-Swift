2ee02a7e175c59875406a3eec6e60e6477c5868f
Artur Shinkevich <archican91@gmail.com>
2017-01-13 02:02:57 +0800

    Add Carthage support

---
+++ FontAwesomeSwift/Example/AF Swift/IconCell.swift
@@ -0,0 +1,18 @@
+//
+//  IconCell.swift
+//  FA Swift
+//
+//  Created by Patrik Vaberer on 7/14/15.
+//  Copyright (c) 2015 Patrik Vaberer. All rights reserved.
+//
+
+import UIKit
+
+class IconCell: UITableViewCell {
+
+    @IBOutlet weak var iIcon: UIImageView!
+    @IBOutlet weak var lBig: UILabel!
+    @IBOutlet weak var lMedium: UILabel!
+    @IBOutlet weak var lSmall: UILabel!
+    @IBOutlet weak var lFont: UILabel!
+}
---
+++ FontAwesomeSwift/Example/AF SwiftTests/AF_SwiftTests.swift
@@ -0,0 +1,36 @@
+//
+//  AF_SwiftTests.swift
+//  AF SwiftTests
+//
+//  Created by Patrik Vaberer on 7/13/15.
+//  Copyright (c) 2015 Patrik Vaberer. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class AF_SwiftTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
