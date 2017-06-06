d903866733d14b4657e788fd378b1e183586d59e
John Breen <jvbreen1@gmail.com>
2017-06-06 16:21:04 -0700

    Fix compiler error with optional CGFont

--- Source/FAIcon.swift
+++ Source/FAIcon.swift
@@ -424,7 +424,7 @@ private class FontLoader {
                 let font = CGFont(provider!)
                 
                 var error: Unmanaged<CFError>?
-                if !CTFontManagerRegisterGraphicsFont(font, &error) {
+                if let font = font, !CTFontManagerRegisterGraphicsFont(font, &error) {
                     
                     let errorDescription: CFString = CFErrorCopyDescription(error!.takeUnretainedValue())
                     let nsError = error!.takeUnretainedValue() as AnyObject as! NSError
